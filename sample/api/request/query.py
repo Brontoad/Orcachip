bff88d5b5c1365225fd592c717b4c67e9bc8b6a8
Brontoad <brontoadgames@gmail.com>
2026-08-17 22:25:48 +0800

    Deprecated sample project

--- sample/api/request/query.py
+++
@@ -1,8 +0,0 @@
-from api.request import request
-class QueryRequest(request.Request): 
-    """
-        Requests containing the search query.
-        Properties:
-            query: str
-    """
-    query: str
